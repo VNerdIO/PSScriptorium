f484742a67c91915951e49ee61d950ce23226ebc
Mike Smith <mosmith@cspire.com>
2019-08-30 08:37:13 -0400

    initial commit

---
+++ PSScriptorium/Public/Get-PSScriptorium.ps1
@@ -0,0 +1,35 @@
+﻿<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .EXAMPLE
+
+    .PARAMETER
+    
+    .OUTPUTS
+
+    .NOTES
+
+    .LINK
+#>
+Function Get-PSScriptorium{
+	[CmdletBinding()]
+	Param([Parameter(Mandatory=$true)]
+		  $Name)
+	
+	begin{}
+	process{
+		try{}
+		catch{
+			$ErrorMessage = $_.Exception.Message
+			$FailedItem = $_.Exception.ItemName
+			
+			Write-Output "$FailedItem - $ErrorMessage"
+			Return $False
+		}
+		finally{}
+	}
+	end{}
+}
+
